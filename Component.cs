340bd240d0a4e62552d0499070f5bd27dd579d5e
r-bin <r-bin@github.fake>
2020-08-14 21:26:46 +0200

    ported settings to ASL basic settings

--- Component.cs
+++ Component.cs
@@ -142,13 +142,6 @@ namespace LiveSplit.UI.Components
             }
         }
 
-        class Settings
-        {
-            public bool Debug { get; set; }
-            public bool ResetHardware { get; set; }
-            public bool HideConnectionBar { get; set; }
-        }
-
         class Game
         {
             public string Name { get; set; }
@@ -332,7 +325,7 @@ namespace LiveSplit.UI.Components
 
             if (_usb2snes.Connected())
             {
-                if (_game?.Settings?.ResetHardware ?? false)
+                if (_aslSettings.GetBasicSettingValue("resethardware"))
                 {
                     _usb2snes.Reset();
                 }
@@ -483,6 +476,8 @@ namespace LiveSplit.UI.Components
         private void SetSplitList()
         {
             var aslSettings = new ASLSettings();
+            aslSettings.AddBasicSetting("resethardware");
+            aslSettings.AddBasicSetting("debug");
             if (_autostart != null)
             {
                 aslSettings.AddBasicSetting("start");
@@ -627,7 +622,7 @@ namespace LiveSplit.UI.Components
                 Console.WriteLine("Get address failed to return result");
                 return false;
             }
-            return split.Check(data, _game?.Settings?.Debug ?? false);
+            return split.Check(data, _aslSettings.GetBasicSettingValue("debug"));
         }
 
         #endregion
@@ -642,11 +637,6 @@ namespace LiveSplit.UI.Components
 
         public void DrawVertical(Graphics graphics, LiveSplitState state, float width, Region clipRegion)
         {
-            if(_game?.Settings?.HideConnectionBar ?? false)
-            {
-                return;
-            }
-
             VerticalHeight = 3 + PaddingTop + PaddingBottom;
             HorizontalWidth = width;
             Color color;
